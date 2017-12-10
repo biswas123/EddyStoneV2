7a84408ea22416b13fe03dc2aa38c8a76c850750
biswas123 <biswas.khayargoli@gmail.com>
2017-12-10 10:29:29 +0545

    First Commit

--- www/js/beaconScanner.js
+++ www/js/beaconScanner.js
@@ -113,8 +113,9 @@
 		var ratio = rssi2 * 1.0 / (txPower2 - 41)
 
 		if (ratio < 1.0) {
-			var accuracy = Math.pow(ratio, 10).toFixed(2); 
-			return accuracy * 100;
+			var accuracy = Math.pow(ratio, 10);
+			accuracy = accuracy.toFixed(2); 
+			return accuracy * 100; 
 		}
 		else {
 			var accuracy = (0.89976) * Math.pow(ratio, 7.7095) + 0.111
